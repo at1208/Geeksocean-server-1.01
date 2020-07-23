d7406e5230451071d3c75c2ca67d27a9d73e96b4
at1208 <amantiwari12081999@gmail.com>
2020-07-23 21:57:50 +0530

    some changes

--- controllers/blog.js
+++ controllers/blog.js
@@ -312,7 +312,7 @@ exports.photo = (req, res) => {
 
 exports.listRelated = (req, res) => {
     // console.log(req.body.blog);
-    let limit = req.body.limit ? parseInt(req.body.limit) : 4;
+    let limit = req.body.limit ? parseInt(req.body.limit) : 3;
     const { _id, categories } = req.body.blog;
 
     Blog.find({ _id: { $ne: _id }, categories: { $in: categories } })
